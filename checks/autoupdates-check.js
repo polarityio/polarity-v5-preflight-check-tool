2f6db123c3dc1241b199defb8ad299fa14305f13
Ed <ed@polarity.io>
2023-12-21 16:21:35 -0500

    Check server version when checking auto updates

--- checks/autoupdates-check.js
+++ checks/autoupdates-check.js
@@ -1,8 +1,19 @@
 const _ = require('lodash');
+const { rpmSearch } = require('../utils');
+const semver = require('semver');
 
 const name = 'Auto-Updates Check';
 
 async function check(env, config, polarity, pgClient, polarityPath, logger) {
+  try {
+    const packageJson = require(`${polarityPath}/package.json`);
+    if (semver.lt(packageJson.version, '4.3.0')) {
+      return `  Auto-Updates not supported in this version of Polarity (Server: ${packageJson.version})`;
+    }
+  } catch (err) {
+    console.warn('Warn: Could not find server package.json to determine server version');
+  }
+
   const setting = await polarity.getServerSetting('autoupdates');
   let enabled = _.get(setting, 'data.attributes.value.enabled', 'Not Set');
   let url = _.get(setting, 'data.attributes.value.url', 'Not Set');
